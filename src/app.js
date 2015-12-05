4abca896bc22e48f698bd2d39760a926ffc6e659
ea54595 <ea54595@gmail.com>
2015-12-05 14:53:02 +0900

    Modified: added babel-pre-set-stage3 for async await.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 import 'babel-polyfill';
 
-var config = require('../config'),
-  co = require('co'),
-  MarketoAuth = require('../lib/marketo-auth'),
-  Sansan = require('../lib/Sansan');
+import config from '../config';
+import Marketo from '../lib/marketo';
+import Sansan from '../lib/sansan';
 
-var marketoAuth = new MarketoAuth(
-  config.identityEndPoint,
-  config.clientId,
-  config.clientSeacret);
+var marketo = new Marketo(
+  config.marketo.restEndPoint,
+  config.marketo.identityEndPoint,
+  config.marketo.clientId,
+  config.marketo.clientSeacret);
 var sansan = new Sansan(config.sansanApiKey);
 
-co(function *(){
-  var bizCards = yield sansan.getBizcards(
-    new Date('2015-1-1'),
-    new Date('2015-2-1')
-  );
-  var auth = yield marketoAuth.getAccessToken();
-  var token = auth.access_token;
-});
+(async () => {
+  let token = await marketo.getAccessToken();
+  marketo.setAccessToken(token.access_token);
+
+  var leadCandidates = [];
+  while(true){
+    let bizCards = await sansan.getBizcards(new Date('2011-01-1'), new Date('2015-11-1'));
+    leadCandidates = leadCandidates.concat(bizCards.data);
+    if (bizCards.hasMore) continue;
+    break;
+  }
+
+  let leads = [];
+  leadCandidates.forEach((bizCard)=>{
+    leads.push({
+      lastName: bizCard.lastName,
+      firstName: bizCard.firstName,
+      email: bizCard.email
+    });
+  });
+
+  let createdResult = await marketo.createLeads(leads);
+  console.log(createdResult);
+})();
